c2d6b1a73c6cae8bf0d81f2e980264a63ba9c8d6
Wellsa15 <wellsa15@unlv.nevada.edu>
2016-03-16 12:15:03 -0700

    DAT2C

--- DA2/DA2T2/DA2T2/main.c
+++ DA2/DA2T2/DA2T2/main.c
@@ -8,22 +8,25 @@
 #include <avr/io.h>
 
 
-int DELAY(int);
 
+void DELAY();
+
+unsigned char a = 1;
+//unsigned char b = 0;
+unsigned char num = 0;
+unsigned char byfive = 0;
 
 int main(void)
 {
 	DDRB = 0xFF;
 	DDRC = 0x31;
-	int b = 0x00;
-	int num = 0x0;
-	int byfive = 0x0;
-	//unsigned char k;
- 
+	//unsigned char num = 0;
+	//int byfive = 0;
+	 
     while (1) 
     {		
-		b = DELAY(b);
-		if (b == 1)
+		DELAY();
+		if ((PORTC % 2) == 1)
 		{
 			num++;
 			byfive++;
@@ -31,44 +34,32 @@ int main(void)
 			
 			if (byfive == 5)
 			{
-				//k = 0x20;
-				//k = k | b;	
-				PORTC4 << 1;
-				PORTC5 << 0;			
+				PORTC |= 1 << PORTC4;
+				PORTC &= 0 << PORTC5;			
 			}			
 			if (byfive == 10)
 			{
-				//k = 0x10;
-				//k = k | b;
-				PORTC4 << 0;
-				PORTC5 << 1;
+				PORTC &= 0 << PORTC4;
+				PORTC |= 1 << PORTC5;
 			}
 			if (byfive == 15)
 			{
-				//k = 0x30;
-				//k = k | b;
-				PORTC4 << 1;
-				PORTC5 << 1;				
+				PORTC |= 1 << PORTC4;
+				PORTC |= 1 << PORTC5;				
 			}
 			if (byfive == 20)
 			{
 				byfive = 0;
-				//k = 0x0;
-				//k = k| b;
-				PORTC4 << 0;
-				PORTC5 << 0;
+				PORTC &= 0 << PORTC4;
+				PORTC &= 0 << PORTC5;
 			}
 	}
-		
-		
     }
 return 0;
 }
 
-int DELAY(int b)
+void DELAY()
 {
-	
-	int a = 0x01;
 	TCNT1 = 0xF862;
 	TCCR1A = 0x0;
 	TCCR1B = 0x05;
@@ -77,15 +68,10 @@ int DELAY(int b)
 	{		
 	}
 	
-	b = a ^ b;
+	//b = a ^ b;
+	
 	TIFR1 |= 1 << 0;
-	if (b == 0)
-	{
-		
-		PORTC0 << 0;
-	}else
-	{
-		PORTC1 << 1;
-	}
-return b;	
+	PORTC ^= a << PORTC0;
+	
+return;	
 }
