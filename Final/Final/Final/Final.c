2449abea732ee25e3abe7619d26ecfa0eead052a
Wellsa15 <wellsa15@unlv.nevada.edu>
2016-05-10 17:30:08 -0700

    Final Complete

--- Final/Final/Final/Final.c
+++ Final/Final/Final/Final.c
@@ -21,18 +21,16 @@
 void USARTstart(unsigned int ubrr); // Function to initialize the USART port
 void USART_tx_string(char *data); // Function to transmit the value over the serial port
 
-char Xout[10]; // output buffer for X-axis
-char Yout[10]; // output buffer for y-axis
-char Zout[10];// output buffer for z-axis
-char DATA_STR[100];
-char WIFI_COMMAND[] = "AT+CWJAP=\"XXX\",\"xxx\"\r\n\0";
-char CONNECT_COMMAND[] = "AT+CIPSTART=\"TCP\",\"184.106.153.149\",80\r\n\0";
-char SEND_COMMAND[20];
-// char SSID[]= "\"XXX\",";
-// char PASSWORD[]= "\"xxx\"\r\n\0";
-// char CLOUDIP[]= "184.106.153.149";
-// char PORTNUM[]= "\",80\r\n";
-// char APIKEY[]= "U6KEQ2CNJNAK85WX";
+char Xout[10]; // output buffer for Acceleration X-axis
+char Yout[10]; // output buffer for Acceleration y-axis
+char Zout[10];// output buffer for Acceleration z-axis
+char GyroXout[10]; // output buffer for Gyro X axis
+char GyroYout[10]; // output buffer for Gyro Y axis
+char GyroZout[10]; // output buffer for Gyro Z axis
+char DATA_STR[120]; // buffer for string that sends data to cloud
+char WIFI_COMMAND[] = "AT+CWJAP=\"XXX\",\"xxx\"\r\n\0"; // String to connect to Wifi
+char CONNECT_COMMAND[] = "AT+CIPSTART=\"TCP\",\"184.106.153.149\",80\r\n\0";// Command to connect to Thingspeak.com
+char SEND_COMMAND[20]; // Buffer for string to tell wifi how many characters will be sent
 // Wifi connect:  AT+CWJAP="SSID", "PASSWORD"
 // Connect to Thinkspeak: AT+CIPSTART= "TCP","%s(IP)" , 80 *Add \ in front of " for it to be read properly 
 // Send data: AT+CIPSEND=(string.length())
@@ -40,18 +38,21 @@ char SEND_COMMAND[20];
 
 int main(void)
 {
-	int counter;
-	uint8_t ret;
-	int16_t accel_buff[3];
-	double accelX, accelY, accelZ;
-	
+	int counter; // Counter for number of characters to be sent
+	uint8_t ret; 
+	int16_t accel_buff[3];// buffer to hold acceleration raw data
+	int16_t gyro_buff[3]; // buffer to hold gyroscope raw data
+	double accelX, accelY, accelZ; // Holds calculated acceleration values 
+	double gyroX, gyroY, gyroZ; // Holds calculated gyroscope values
 	sei (); // enable global interrupts
 	USARTstart(UBRR_115200); // initialize the USART port
 	i2c_init();// initialize I2C
 	
-	//Build command connect to Wifi and transmit
+//	Build command connect to Wifi and transmit
 // 	strcat(WIFI_COMMAND,SSID);
 // 	strcat(WIFI_COMMAND,PASSWORD);
+//	USART_tx_string(WIFI_COMMAND);// Connect to Wifi
+
 	DDRB |= (1<<5);
 		
 	//initialize & test MPU5060 availability
@@ -65,24 +66,27 @@ int main(void)
 
 	mpu6050_init();// Initialize MPU
 	
-	//USART_tx_string(WIFI_COMMAND);// Connect to Wifi
-	
-	// Build Command to connect to Cloud
-// 	strcat(CONNECT_COMMAND,CLOUDIP);
-// 	strcat(CONNECT_COMMAND,PORTNUM);
-	for (int b=0; b<10;b++)
+
+
+	for (int b=0; b<10;b++) // Delay to give wifi time to connect and be ready
 		_delay_ms(1000);
 		
 	while (1) // infinite loop to transmit the sensor value over serial port
 	{
 		counter = 0;
-		mpu6050_read_accel_ALL(accel_buff);
+		mpu6050_read_accel_ALL(accel_buff);// Read acceleration data
+		mpu6050_read_gyro_ALL(gyro_buff);// Read gyroscopic data
 			
 		// acceleration (m/s^2)
-		accelX = accel_buff[0]/16384.0;
-		accelY = accel_buff[1]/16384.0;
-		accelZ = accel_buff[2]/16384.0;
+		accelX = accel_buff[0]/16384.0; // Calculate Acceleration X-axis value
+		accelY = accel_buff[1]/16384.0; // Calculate Acceleration Y-axis value
+		accelZ = accel_buff[2]/16384.0; // Calculate Acceleration Z-axis value
+		//
+		gyroX = gyro_buff[0]/32.8; // Calculate Gyroscopic X-axis value
+		gyroY = gyro_buff[1]/32.8; // Calculate Gyroscopic Y-axis value
+		gyroZ = gyro_buff[2]/32.8; // Calculate Gyroscopic Z-axis value
 		
+		//Convert Acceleration X,Y, and Z values to Strings
 		if (accelX < 0)
 			dtostrf(accelX, 6, 3, Xout);
 		else
@@ -103,12 +107,35 @@ int main(void)
 		{
 			dtostrf(accelZ, 6, 3, Zout);
 			memmove(Zout, Zout+1,sizeof(Zout));
-		}		
-
+		}
+		
+		// Convert Gyroscopic X,Y, and Z values to strings		
+		if (gyroX < 0)
+			dtostrf(gyroX, 6, 3, GyroXout);
+		else
+		{
+			dtostrf(gyroX, 6, 3, GyroXout);
+			memmove(GyroXout, GyroXout+1,sizeof(GyroXout));
+		}
+		if (gyroY < 0)
+		dtostrf(gyroY, 6, 3, GyroYout);
+		else
+		{
+			dtostrf(gyroY, 6, 3, GyroYout);
+			memmove(GyroYout, GyroYout+1,sizeof(GyroYout));
+		}
+		if (gyroZ < 0)
+		dtostrf(gyroZ, 6, 3, GyroZout);
+		else
+		{
+			dtostrf(gyroZ, 6, 3, GyroZout);
+			memmove(GyroZout, GyroZout+1,sizeof(GyroZout));
+		}
+		
 		// Build Data string to send to cloud
-		snprintf(DATA_STR, sizeof(DATA_STR),"GET /update?api_key=U6KEQ2CNJNAK85WX&field1=%s&field2=%s&field3=%s\r\n", Xout, Yout, Zout);
+		snprintf(DATA_STR, sizeof(DATA_STR),"GET /update?api_key=U6KEQ2CNJNAK85WX&field1=%s&field2=%s&field3=%s&field4=%s&field5=%s&field6=%s\r\n", Xout, Yout, Zout, GyroXout, GyroYout, GyroZout);
 	
-		while (DATA_STR[counter] != '\0')
+		while (DATA_STR[counter] != '\0') // Count how many characters in the output string
 		{
 			counter++;
 		}
@@ -116,9 +143,9 @@ int main(void)
 		snprintf(SEND_COMMAND, sizeof(SEND_COMMAND),"AT+CIPSEND=%d\r\n",counter);
 
 		USART_tx_string(CONNECT_COMMAND);// Connect to Thinkspeak.com
-		_delay_ms(500);
+		_delay_ms(400);
 		USART_tx_string(SEND_COMMAND);// Transmit command to send data
-		_delay_ms(200);
+		_delay_ms(100);
 		USART_tx_string(DATA_STR); // transmit data over serial connection
 		for (int i = 0; i<16; i++)// Wait 15sec to upload next dataset
 			_delay_ms(1000);
@@ -149,38 +176,3 @@ void USART_tx_string(char *data)// transmit value function
 		data++; // go to next character
 	}
 }
-/*
-void i2c_init()
-{
-	TWSR= 0x00; // set prescaler bits to zero
-	TWBR= 0x0C;	// SCL Freq is 400k for XTAL = 16M
-	TWCR= 0x04; // Enable the TWI module	
-}
-
-void i2c_start2()
-{
-	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
-	while ((TWCR &(1<<TWINT)) == 0);
-}
-
-void i2c_write(unsigned char data)
-{
-	TWDR = data;
-	TWCR = (1<<TWINT)|(1<<TWEN);
-	while((TWCR&(1<<TWINT)) ==0);
-}
-
-void i2c_read(unsigned char	isLast)
-{
-	if (isLast == 0) // if want to read more than 1 byte
-		TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA);
-	else
-		TWCR = (1<<TWINT)|(1<<TWEN);
-	while ((TWCR &(1<<TWINT)) == 0);
-return TWDR;
-}
-
-void i2c_stop()
-{
-	TWCR = (1<<TWINT)|(1<<TWEN)|(TWSTO);
-}*/
